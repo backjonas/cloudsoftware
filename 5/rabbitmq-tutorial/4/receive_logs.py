7300894dcd8a8ea52b25aac2a0a8a0cb907a2c11
Jonas Bäck <jonas.c.back@gmail.com>
2022-11-08 16:00:43 +0200

    rabbitmq tutorial

---
+++ 5/rabbitmq-tutorial/4/receive_logs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import pika
+import sys
+import os
+
+def main():
+  def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+  connection = pika.BlockingConnection(
+    pika.ConnectionParameters('localhost'))
+  channel = connection.channel()
+
+  channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+  
+  result = channel.queue_declare(queue='', exclusive=True)
+  queue_name = result.method.queue
+
+  severities = sys.argv[1:]
+  if not severities:
+    sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
+    sys.exit(1)
+
+  for severity in severities:
+    channel.queue_bind(
+      exchange='direct_logs', queue=queue_name, routing_key=severity)
+
+  channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+  print(' [*] Waiting for messages. To exit press CTRL+C')
+  channel.start_consuming()
+
+if __name__ == '__main__':
+  try:
+    main()
+  except KeyboardInterrupt:
+    print('Interrupted')
+    try:
+      sys.exit(0)
+    except SystemExit:
+      os._exit(0)
